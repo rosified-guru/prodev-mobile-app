40f39b2f35d32cae0b381d5890d78918ef8fe66f
Rose Mwangi <muthonirosee7@gmail.com>
2025-07-20 22:24:19 +0300

    Styling with NativeWind

---
+++ prodev-mobile-app-0x06/app/join.tsx
@@ -0,0 +1,92 @@
+import { Text, TextInput, View, TouchableOpacity, Image } from "react-native";
+import { styles } from "@/styles/_joinstyle";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import { FontAwesome, Ionicons } from "@expo/vector-icons";
+import { Link, router } from "expo-router";
+
+export default function Join() {
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        {/* Navigation and Logo */}
+        <View style={styles.navGroup}>
+          <TouchableOpacity onPress={() => router.back()}>
+            <Ionicons name="arrow-back" size={25} />
+          </TouchableOpacity>
+          <Image source={require('@/assets/images/logo-green.png')} />
+        </View>
+
+        {/* Headings */}
+        <Text style={styles.largeText}>Create your</Text>
+        <Text style={styles.largeText}>Account</Text>
+        <Text style={styles.smallText}>
+          Enter your details below to sign up.
+        </Text>
+
+        {/* Form Group */}
+        <View style={styles.formGroup}>
+          <View>
+            <Text style={styles.placeholderText}>Full Name</Text>
+            <TextInput style={styles.inputField} />
+          </View>
+
+          <View style={{ marginTop: 20 }}>
+            <Text style={styles.placeholderText}>Email</Text>
+            <TextInput keyboardType="email-address" style={styles.inputField} />
+          </View>
+
+          <View style={{ marginTop: 20 }}>
+            <Text style={styles.placeholderText}>Password</Text>
+            <View style={styles.passwordGroup}>
+              <TextInput style={{ flex: 1 }} secureTextEntry />
+              <FontAwesome name="eye-slash" size={24} color="#7E7B7B" />
+            </View>
+          </View>
+        </View>
+
+        {/* Join Button */}
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Join now</Text>
+        </TouchableOpacity>
+
+        {/* Divider */}
+        <View style={styles.dividerGroup}>
+          <View style={styles.divider}></View>
+          <Text style={styles.dividerText}>OR</Text>
+          <View style={styles.divider}></View>
+        </View>
+
+        {/* Social Media Options */}
+        <View style={styles.socialMediaButtonGroup}>
+          <TouchableOpacity style={styles.socialMediaButton}>
+            <View style={{ flexDirection: "row", alignItems: "center", gap: 5 }}>
+              <Image source={require("@/assets/images/google.png")} />
+              <Text style={styles.socialMediaButtonText}>
+                Continue with Google
+              </Text>
+            </View>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.socialMediaButton}>
+            <View style={{ flexDirection: "row", alignItems: "center", gap: 5 }}>
+              <Image source={require("@/assets/images/facebook.png")} />
+              <Text style={styles.socialMediaButtonText}>
+                Continue with Facebook
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+
+        {/* Footer Prompt */}
+        <View style={styles.subTextGroup}>
+          <Text style={styles.subText}>Already have an account?</Text>
+          <Link href="/Sign" asChild>
+            <TouchableOpacity>
+              <Text style={styles.subTextJoin}>Sign in</Text>
+            </TouchableOpacity>
+          </Link>
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
